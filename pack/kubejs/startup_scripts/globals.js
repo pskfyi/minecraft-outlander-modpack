3ce5b041d3430ce1c4346907b97754eb1aa867b9
pskfyi <patrick.keenan.fyi@gmail.com>
2024-04-19 01:32:14 -0700

    config(pack): remove stone tools

--- pack/kubejs/startup_scripts/globals.js
+++ pack/kubejs/startup_scripts/globals.js
@@ -8,7 +8,13 @@ global.items = {
     "minecraft:wooden_pickaxe",
     "minecraft:wooden_axe",
     "minecraft:wooden_hoe",
+    "minecraft:stone_sword",
+    "minecraft:stone_shovel",
+    "minecraft:stone_pickaxe",
+    "minecraft:stone_axe",
+    "minecraft:stone_hoe",
     "aquaculture:wooden_fillet_knife",
     "exnihilosequentia:wooden_hammer",
+    "ftbquests:loot_crate_opener",
   ]
 }
