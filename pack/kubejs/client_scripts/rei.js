61e26d836b73d9a9f0fe986a33dfefdf0d97604f
pskfyi <patrick.keenan.fyi@gmail.com>
2024-04-20 05:13:14 -0700

    config(pack): add custom JEI category for Natural Progression knapping

--- pack/kubejs/client_scripts/rei.js
+++ pack/kubejs/client_scripts/rei.js
@@ -4,6 +4,43 @@
 //   event.yeet("minecraft:plugins/tag");
 // })
 
+// Adding custom
+JEIAddedEvents.registerCategories((event) => {
+  event.custom("natprog:knapping", (category) => {
+    const guiHelper = category.jeiHelpers.guiHelper;
+ 
+    category
+      .title("Knapping")
+      .background(guiHelper
+        .createBlankDrawable(150, 100))
+      .icon(guiHelper
+        .createDrawableItemStack(Item.of("minecraft:flint")))
+      .isRecipeHandled(() => true)
+      .handleLookup((builder, recipe) => {
+        builder
+          .addSlot("INPUT", 67, 10)
+          .addItemStack(Item.of(recipe.data.name))
+          .setSlotName("input");
+        builder
+          .addInvisibleIngredients("OUTPUT")
+          .addItemStack(Item.of(recipe.data.name));
+      })
+      .setDrawHandler((recipe, _recipeSlotsView, guiGraphics) => {
+        guiGraphics
+          .drawWordWrap(Client.font, Text.of(recipe.data.description), 2, 36, 146, 0);
+      });
+  });
+});
+ 
+JEIAddedEvents.registerRecipes((event) => {
+  event
+    .custom("natprog:knapping")
+    .add({
+      name: "minecraft:flint",
+      description: "Hold pebbles in both hands, then right-click to clack them together. There is a chance that they will produce sharp flint."
+    })
+});
+
 REIEvents.hide('item', event => {
   global.items.remove.map(id => event.hide(id))
 })
