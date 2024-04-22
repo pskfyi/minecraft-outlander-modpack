2ab13d315be5034e71769ab4bec301accdbb7a4a
pskfyi <patrick.keenan.fyi@gmail.com>
2024-04-21 17:23:34 -0700

    config(pack): more early game quests; altered textile recipes

---
+++ pack/kubejs/server_scripts/stage1/textiles.js
@@ -0,0 +1,65 @@
+// priority: 10
+
+const STRING = "minecraft:string"
+const CANVAS = "farmersdelight:canvas"
+const STRAW = "farmersdelight:straw"
+const ROPE = "farmersdelight:rope"
+const SACK = "supplementaries:sack"
+const FLAX = "supplementaries:flax"
+
+ServerEvents.recipes((event) => {
+  // TODO: Pick 1 rope and get all recipes to use it.
+  // TODO: Canvas should be craftable at a sewing table.
+
+  // These recipes have the same ID as the items they produce.
+  event.remove({ id: ROPE })
+  event.remove({ id: SACK })
+
+  event.shaped( // TODO: Remove this when changing Canvas crafting.
+    CANVAS,
+    [
+      "FF ",
+      "FF ",
+    ],
+    {
+      F: FLAX,
+    }
+  )
+
+  event.shaped(
+    Item.of(ROPE, 3),
+    [
+      "TT ",
+      "TT ",
+      "TT ",
+    ],
+    {
+      T: STRAW,
+    }
+  )
+
+  event.shaped(
+    STRING,
+    [
+      "T  ",
+      "T  ",
+      "T  ",
+    ],
+    {
+      T: STRAW,
+    }
+  )
+
+  event.shaped(
+    SACK,
+    [
+      "CSC",
+      "C C",
+      "CCC",
+    ],
+    {
+      C: CANVAS,
+      S: STRING,
+    }
+  )
+})
